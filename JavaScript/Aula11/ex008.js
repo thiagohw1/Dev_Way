f1442b33b6fa16b59eb633d77b2f61bd8c0b5691
thiagohw1 <thiagohw1@gmail.com>
2023-06-08 00:00:28 -0300

    Create ex008.js

---
+++ JavaScript/Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`Digira sempre com o cinto de segurança`)
+console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+
+if (vel > 80){
+    console.log(`A velocidade do seu carro é ${vel}, você ultrapassou o limite de velocidade`)
+}
