bc6bfb2235fee8a2b3a6618cd4010f98cb8f87c2
thiagohw1 <thiagohw1@gmail.com>
2023-06-09 15:02:06 -0300

    09/06/23 15:02

---
+++ JavaScript/Aula12/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`são ${hora} horas`)
+
+if (hora<12){
+    console.log('bom dia')
+}else if(hora>12){
+    console.log('boa tarde')
+}else{
+    console.log('boa noite')
+}
